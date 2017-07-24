7356c2b82105f06bd09ddf3babe25e64186baaa8
Victor Wilm <victorrw@msn.com>
2017-07-24 17:22:01 -0300

    Fix

--- config.rb
+++ config.rb
@@ -27,6 +27,10 @@ activate :autoprefixer
 #   page "/admin/*"
 # end
 
+data.flats.each do |name|
+  proxy "/flats/#{name}.html", "/source/show.html", :locals => { :owner => name }, :ignore => true
+end
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
